f9ee941e058eb898ce864fb3de592d6d1d0c5e33
Kirill Dashkevich <programmerdashkevich@gmail.com>
2023-03-11 17:04:13 +0300

    create hide and show password

--- features/entry/src/main/java/com/dashkevich/entry/login/LoginFragment.kt
+++ features/entry/src/main/java/com/dashkevich/entry/login/LoginFragment.kt
@@ -1,6 +1,9 @@
 package com.dashkevich.entry.login
 
 import android.os.Bundle
+import android.text.InputType
+import android.text.method.HideReturnsTransformationMethod
+import android.text.method.PasswordTransformationMethod
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -11,11 +14,24 @@ import com.dashkevich.entry.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment(R.layout.fragment_login) {
 
-    lateinit var binding: FragmentLoginBinding
+    private lateinit var binding: FragmentLoginBinding
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentLoginBinding.bind(view)
 
+
+        binding.visibilityPassword.setOnClickListener {
+            val password = binding.password
+            password.inputType =
+                if (password.inputType == InputType.TYPE_CLASS_TEXT) {
+                    password.transformationMethod = PasswordTransformationMethod()
+                    InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD
+                } else {
+                    password.transformationMethod = HideReturnsTransformationMethod()
+                    InputType.TYPE_CLASS_TEXT
+                }
+            password.setSelection(password.length())
+        }
     }
 }
