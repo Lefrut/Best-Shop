ee0323ea23eeb34b95f8480915abb5b03c67b4aa
Kirill Dashkevich <programmerdashkevich@gmail.com>
2023-03-24 14:42:23 +0300

    Create log in BlankFragment.kt

--- navigation/src/main/java/com/dashkevich/navigation/BlankFragment.kt
+++ navigation/src/main/java/com/dashkevich/navigation/BlankFragment.kt
@@ -11,6 +11,7 @@ class BlankFragment: Fragment(R.layout.fragment_blank) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         Log.e("Navigation", "LoginFragment, onViewCreated: create", )
+        Log.e("Navigation", "LoginFragment, onViewCreated: bug fix", )
         findNavController().navigate(R.id.action_global_login_nav_graph)
     }
 
